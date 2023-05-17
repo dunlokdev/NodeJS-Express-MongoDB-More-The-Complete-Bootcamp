e5d942035f655b077bfc3d1e7a4a76ce9b228928
dunlok <myloc442@gmail.com>
2023-05-17 16:49:05 +0700

    Building a (Very) Simple API

---
+++ 1-node-farm/starter/index.js
@@ -0,0 +1,70 @@
+const fs = require("fs");
+const http = require("http");
+const hello = "hello world";
+
+console.log(hello);
+
+///////////////////////////////
+// FILES
+
+// Blocking, synchronous way
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+// const textOut = `This is what we know about avocado: ${textIn}. \nCreate on: ${
+//   Date.now() * 1000
+// }`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File written !");
+
+// Non-blocking, asynchronous way
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   if (err) return console.log("Error !, ", err);
+
+//   console.log(data1);
+
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+
+//     fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+//       console.log(data3);
+
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf8", (err) => {
+//         console.log("Your file has been written !");
+//       });
+//     });
+//   });
+// });
+
+/////////////////////////////////////
+// SERVER
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+console.log("chay vo day nuk roi hihi");
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+
+  const pathName = req.url;
+  console.log("🚀 ~ server ~ pathName:", pathName);
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW");
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+  } else if (pathName === "/api") {
+    // read data from file return json
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page not found</h1>");
+  }
+});
+
+server.listen(8000, "localhost", () => {
+  console.log("Listening to request on port 8000!");
+});
