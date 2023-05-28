600e2a01c454593a6aa4b70e891ba0db75e8bffb
dunlok <myloc442@gmail.com>
2023-05-28 20:11:07 +0700

    Responding to URL Parameters

--- 2-natours-app/app.js
+++ 2-natours-app/app.js
@@ -25,24 +25,38 @@ app.get('/api/v1/tours', (req, res) => {
 	})
 })
 
-app.post('/api/v1/tours', (req, res) => {
-	console.log(req.body)
-	const newId = tours[tours.length - 1].id + 1
-	const newTour = Object.assign({ id: newId }, req.body)
-	tours.push(newTour)
-
-	// Add new tour
-	fs.writeFile(
-		`${__dirname}/dev-data/data/tours-simple.json`,
-		JSON.stringify(tours),
-		(err) => {
-			res.status(201).json({
-				status: 'success',
-				tours: newTour,
-			})
-		}
-	)
+app.get('/api/v1/tours', (req, res) => {
+	res.status(200).json({
+		status: 'success',
+		results: tours.length,
+		data: {
+			tours,
+		},
+	})
 })
+
+app.get('/api/v1/tours/:id', (req, res) => {
+	console.log(req.params)
+
+	const id = req.params.id * 1
+
+	const tour = tours.find((el) => el.id === id)
+
+	if (!tour) {
+		return res.status(400).json({
+			status: 'fail',
+			message: 'Invalid an id, try again',
+		})
+	}
+
+	res.status(200).json({
+		status: 'success',
+		data: {
+			tour,
+		},
+	})
+})
+
 const port = 3000
 app.listen(port, () => {
 	console.log(`App running on port ${port}`)
